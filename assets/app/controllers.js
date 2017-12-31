cfbf34a7bf06bf6280d94ae64418ef80cdb52149
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2017-12-31 22:03:11 +0300

    Added favStorage, glyphicons for Favorites

--- assets/app/controllers.js
+++ assets/app/controllers.js
@@ -5,6 +5,7 @@ if (location.hostname == '127.0.0.1' || location.hostname == 'localhost') {
 
 function MainCtrl ($http, $interval, $scope) {
     var vm = this;
+    var favStorage = JSON.parse(localStorage.favStorage || '{}');
     function fetchRates () {
         $http.get(API_PREFIX + 'rates', {params: {code: ''}}).then(function (response) {
             let rates = [],
@@ -12,14 +13,27 @@ function MainCtrl ($http, $interval, $scope) {
             for (let code in items) {
                 let d = items[code];
                 d.code = code;
+                d.favWeight = favStorage[code] ? 10000 : 1;
                 rates.push(d);
             }
-            rates.sort((a, b) => b.usdt - a.usdt);
+            rates.sort((a, b) => b.favWeight * b.usdt - a.favWeight * a.usdt);
             vm.rates = rates;
         });
     }
     fetchRates();
-    $interval(fetchRates, 1000);
+    $interval(fetchRates, 500000);
+    vm.favClass = function (code) {
+        return favStorage[code] ? 'glyphicon-star gold' : 'glyphicon-star-empty gray';
+    }
+    vm.favClick = function (code) {
+        if (favStorage[code]) {
+            delete favStorage[code];
+        } else {
+            favStorage[code] = {};
+        }
+        localStorage.setItem('favStorage', JSON.stringify(favStorage));
+        return false;
+    }
 }
 
 (function () {
